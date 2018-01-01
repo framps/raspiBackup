a0bb08140b03603685e857c6444af039ba3b3015
framp <framp@linux-tips-and-tricks.de>
2018-01-01 19:51:15 +0100

    Added and updated PATH extension for crontab

--- helper/raspiBackupWrapper.sh
+++ helper/raspiBackupWrapper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Sample script to wrap raspiBackup.sh in order 
+# Sample script to wrap raspiBackup.sh in order
 # to mount and unmount the backup device
 # and start and stop services
 #
@@ -10,13 +10,13 @@
 
 MYSELF=${0##*/}
 MYNAME=${MYSELF%.*}
-VERSION="0.2.2"
+VERSION="0.2.3"
 
-GIT_DATE="$Date: 2016-11-27 17:14:59 +0100$"
+GIT_DATE="$Date: 2017-10-31 12:03:40 +0100$"
 GIT_DATE_ONLY=${GIT_DATE/: /}
 GIT_DATE_ONLY=$(cut -f 2 -d ' ' <<< $GIT_DATE)
-GIT_TIME_ONLY=$(cut -f 3 -d ' ' <<< $GIT_DATE) 
-GIT_COMMIT="$Sha1: 3d7a51a$"
+GIT_TIME_ONLY=$(cut -f 3 -d ' ' <<< $GIT_DATE)
+GIT_COMMIT="$Sha1: ed54e5e$"
 GIT_COMMIT_ONLY=$(cut -f 2 -d ' ' <<< $GIT_COMMIT | sed 's/\$//')
 
 GIT_CODEVERSION="$MYSELF $VERSION, $GIT_DATE_ONLY/$GIT_TIME_ONLY - $GIT_COMMIT_ONLY"
@@ -26,13 +26,13 @@ BACKUP_PATH="$BACKUP_MOUNT_POINT/myraspberries"              # ===> adapt to you
 
 # add pathes if not already set (usually not set in crontab)
 
-if [[ -e /bin/egrep ]]; then
-	PATHES="/bin /sbin /usr/bin /usr/sbin /usr/local/bin /usr/local/sbin"
-	for p in $PATHES; do
-		if ! /bin/egrep "^$p|:$p" <<< "$PATH" 2>&1 1>/dev/null; then
-			[[ -z "$PATH" ]] && export PATH="$p" || export PATH="$p:$PATH"
-		fi
-	done
+if [[ -e /bin/grep ]]; then
+   PATHES="/usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin"
+   for p in $PATHES; do
+      if ! /bin/grep -E -q "[^:]$p[:$]" <<< $PATH; then
+         [[ -z $PATH ]] && export PATH=$p || export PATH="$p:$PATH"
+      fi
+   done
 fi
 
 function trapWithArg() { # function trap1 trap2 ... trapn
@@ -58,7 +58,7 @@ function isMounted() {
 function cleanup() { # trap
 	if (( ! $WAS_MOUNTED )); then
 		echo "--- Unmounting $BACKUP_MOUNT_POINT"
-		umount $BACKUP_MOUNT_POINT		
+		umount $BACKUP_MOUNT_POINT
 	fi
 }
 
@@ -68,7 +68,7 @@ trapWithArg cleanup SIGINT SIGTERM EXIT
 
 # check if mountpoint is mounted
 if ! isMounted $BACKUP_MOUNT_POINT; then
-	WAS_MOUNTED=0	
+	WAS_MOUNTED=0
 	echo "--- Mounting $BACKUP_MOUNT_POINT"
 	mount $BACKUP_MOUNT_POINT	# no, mount it
 else
@@ -80,12 +80,12 @@ fi
 # now stop all active services  					===> adapt to your environment
 
 service nfs-kernel-server stop
-service samba stop 
+service samba stop
 service apache2 stop
 service mysql stop
 
 # create backup
-raspiBackup.sh -p $BACKUP_PATH -a ":" -o ":"      # ===> insert all additional parameters or use /usr/local/etc/raspiBackup.conf to pass all parameters
+raspiBackup.sh -a ":" -o ":" $BACKUP_PATH     	  # ===> insert all additional parameters or use /usr/local/etc/raspiBackup.conf to pass all parameters
 
 rc=$?
 
