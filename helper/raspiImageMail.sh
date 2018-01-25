ff41f2e32bafe4738316ff48aadc5bdb308338aa
framp <framp@linux-tips-and-tricks.de>
2018-01-25 19:42:07 +0100

    Updated raspiImage received from kmbach

--- helper/raspiImageMail.sh
+++ helper/raspiImageMail.sh
@@ -8,12 +8,12 @@
 # Using: raspiImageMail.sh <msgTitle> <msg> [<attach>]
 #        The condition is that mail is configured in raspiBackup.conf and works in raspiBackup.
 #
-# Visit http://www.linux-tips-and-tricks.de/raspiBackup and https://github.com/framps/raspiBackup to get more 
+# Visit http://www.linux-tips-and-tricks.de/raspiBackup and https://github.com/framps/raspiBackup to get more
 # details about raspiBackup
 #
 #######################################################################################################################
 #
-#    Copyright (C) 2017 kmbach 
+#    Copyright (C) 2017 kmbach
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -29,6 +29,54 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 #######################################################################################################################
+#
+# raspiImageMail.sh for raspiBackupRestore2Image.sh
+# =================================================
+# raspiImageMail.sh extends raspiBackupRestore2Image.sh and sends an
+# eMail of it's execution result. The required function to send eMails is
+# used from raspiBackup together with the eMail configuration done in
+# raspiBackup.conf. Finally the console messages of raspiBackupRestore2Image.sh
+# will be sent in an eMail.
+#
+# Base:
+# raspiBackup.sh VERSION="0.6.3.1"
+#
+# Prerequisites:
+# -  raspiImageMail.sh, raspiBackup.sh, raspiBackupRestore2Image.sh and pishrink.sh
+#    have to be in the same directory which defaults to /usr/local/bin
+# -  raspiImageMail.sh has to be executable (chmod +x raspiImageMail.sh)
+# -  /usr/local/etc/raspiBackup.conf has to be configured so raspiBackup.sh
+#    is able to send an eMail
+#
+# If any prerequisites are not fulfilled raspiBackupRestore2Image.sh will not fail.
+# You just will not get an eMail.
+#
+#######################################################################################################################
+#
+# raspiImageMail.sh für raspiBackupRestore2Image.sh
+# =================================================
+# raspiImageMail.sh erweitert raspiBackupRestore2Image.sh um Funktionalitäten
+# zum Versenden von Ergebnis- eMails. Dafür importiert raspiImageMail.sh die für das
+# Versenden von eMail benötigten Funkionen aus raspiBackup.sh sowie die
+# eMail- Einstellungen aus raspiBackup.conf. Die Konsolenmeldungen aus
+# raspiBackupRestore2Image.sh werden in einer eMail verschickt.
+#
+# Basis:
+# raspiBackup.sh VERSION="0.6.3.1"
+#
+# Voraussetzungen:
+# -  raspiImageMail.sh muss sich, zusammen mit den Programmen
+#    raspiBackup.sh, raspiBackupRestore2Image.sh und pishrink.sh,
+#    im gleichen Verzeichnis befinden. Das ist standardmäßig /usr/local/bin
+# -  raspiImageMail.sh muss ausführbar sein (chmod +x raspiImageMail.sh)
+# -  in der /usr/local/etc/raspiBackup.conf sollte eMail konfiguriert
+#    und aus raspiBackup.sh heraus funktionsfähig sein
+#
+# Befindet sich die Datei raspiImageMail.sh nicht im raspiBackup- Verzeichnis, ist nicht
+# ausführbar oder ist die eMail- Funktion in raspiBackup nicht eingerichtet bzw.
+# funktioniert nicht, führt dies zu keinem Fehlverhalten von raspiBackupRestore2Image.
+# Es wird nur einfach keine email generiert. Funktioniert es widererwartend nicht sollten
+# nochmals die Punkte unter 'Voraussetzungen' geprüft werden!
 
 MYSELF=${0##*/}
 
@@ -41,14 +89,14 @@ fi
 
 function usage() {
 	echo "Syntax: $MYSELF <msgTitle> <msg> [<attach>]"
-}	
+}
 
 # query invocation parms
 if [[ $# < 2 ]]; then
 	echo "??? Missing Mail parameter"
 	usage
 	exit 1
-fi	
+fi
 
 MSG_TITLE="$1"
 MSG="$2"
@@ -70,12 +118,12 @@ MYNAME_ABS="$(dirname $0)""/""$MYNAME"".sh"
 NL=$'\n'
 
 # dummys to use the original features of raspiBackup.sh
-function logItem() { : ;} 
-function logExit() { : ;} 
-function logEntry() { : ;} 
-function assertionFailed() { : ;}  
+function logItem() { : ;}
+function logExit() { : ;}
+function logEntry() { : ;}
+function assertionFailed() { : ;}
 
-# functions load from raspiBackup.sh. 
+# functions load from raspiBackup.sh.
 # The closing bracket of the function definition must be at the beginning of the line
 . /dev/stdin <<EOF
 $(sed -n '/^function *readConfigParameters\(\)/,/^}/p' $MYNAME_ABS)
@@ -86,7 +134,7 @@ EOF
 # include raspiBackup.conf
 readConfigParameters
 
-# set parameters from raspiBackup.conf 
+# set parameters from raspiBackup.conf
 EMAIL=$DEFAULT_EMAIL
 EMAIL_PROGRAM=$DEFAULT_MAIL_PROGRAM
 EMAIL_PARMS=$DEFAULT_EMAIL_PARMS
@@ -97,8 +145,8 @@ if [[ $APPEND != "" ]]; then
         APPEND_LOG=1
 	    LOG_FILE="$APPEND"
     else
-        MSG="$MSG$NL$NL$APPEND not exist." 
-    fi    
+        MSG="$MSG$NL$NL$APPEND not exist."
+    fi
 fi
 
 # mail parameters are defined in .conf
@@ -107,7 +155,7 @@ if [[ -n $EMAIL ]]; then
     RC=$?
 else
     RC=1
-fi 
+fi
 
 exit $RC
 
