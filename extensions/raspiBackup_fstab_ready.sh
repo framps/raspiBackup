ca4b649f3e8695a9c65df6c35b024d040f646b9d
framp <framp@linux-tips-and-tricks.de>
2018-10-19 22:13:21 +0200

    Updated cpy

--- extensions/raspiBackup_fstab_ready.sh
+++ extensions/raspiBackup_fstab_ready.sh
@@ -2,35 +2,35 @@
 
 #
 # extensionpoint for raspiBackup.sh
-# called before dd, tar or rsync backup is started 
-# 
+# called before dd, tar or rsync backup is started
+#
 # Function: Copy /etc/fstab into backupdirectory
 #
-# See http://www.linux-tips-and-tricks.de/raspiBackup for additional information 
+# See http://www.linux-tips-and-tricks.de/raspiBackup for additional information
 #
 # (C) 201i7 - framp at linux-tips-and-tricks dot de
 #
 
 # set any messages and prefix message name with ext_ and some unique prefix to use a different namespace than the script
 MSG_EXT_FSTAB_COPY="ext_fstab_copy"
-MSG_EN[$MSG_EXT_FSTAB_COPY]="--- RBK1005I: Copy %1 to %2"
-MSG_DE[$MSG_EXT_FSTAB_COPY]="--- RBK1005I: Kopiere %1 in %2"
+MSG_EN[$MSG_EXT_FSTAB_COPY]="--- RBK1005I: Copy %s to %s"
+MSG_DE[$MSG_EXT_FSTAB_COPY]="--- RBK1005I: Kopiere %s in %s"
 
 FSTAB_FILENAME="/etc/fstab"
 FSTAB_TARGET_DIR="raspiBackup/extensionSaveArea"
 TARGET_DIR="$BACKUPTARGET_DIR/$FSTAB_TARGET_DIR"
 
-# write message to console 
+# write message to console
 writeToConsole $MSG_LEVEL_MINIMAL $MSG_EXT_FSTAB_COPY "$FSTAB_FILENAME" "$TARGET_DIR"
 
 if [[ -f "$FSTAB_FILENAME" ]]; then
 	mkdir -p "$TARGET_DIR"
 	extrc=$?
-	[[ $extrc != 0 ]] && return $extrc	
+	[[ $extrc != 0 ]] && return $extrc
 	cp "$FSTAB_FILENAME" "$TARGET_DIR"
 	ls -la "$TARGET_DIR"
-	return $? 
-else 
+	return $?
+else
 	return 1	// terminate raspiBackup
 fi
 
--- extensions/raspiBackup_mem_post.sh
+++ extensions/raspiBackup_mem_post.sh
@@ -31,13 +31,13 @@ ext_freememory_post=( $(getMemoryFree) )
 
 # set any messages and prefix message name with ext_ and some unique prefix to use a different namespace than the script
 MSG_EXT_DISK_FREE="ext_freememory_1"
-MSG_EN[$MSG_EXT_DISK_FREE]="--- RBK1001I: Memory usage - Pre backup: Used: %1 Free: %2 - Post backup - Used: %3 Free: %4"
-MSG_DE[$MSG_EXT_DISK_FREE]="--- RBK1001I: Speicherauslastung - Vor dem Backup: Belegt: %1 Frei: %2 - Nach dem Backup: Belegt: %3 Frei: %4"
+MSG_EN[$MSG_EXT_DISK_FREE]="--- RBK1001I: Memory usage - Pre backup - Used: %s MB Free: %s MB - Post backup - Used: %s MB Free: %s MB"
+MSG_DE[$MSG_EXT_DISK_FREE]="--- RBK1001I: Speicherauslastung - Vor dem Backup - Belegt: %s MB Frei: %s MB - Nach dem Backup: Belegt: %s MB Frei: %s MB"
 
 # now write message to console and log and email
 # $MSG_LEVEL_MINIMAL will write message all the time
 # $MSG_LEVEL_DETAILED will write message only if -m 1 parameter was used
-writeToConsole $MSG_LEVEL_MINIMAL $MSG_EXT_DISK_FREE "$(bytesToHuman ${ext_freememory_pre[0]})" "$(bytesToHuman ${ext_freememory_pre[1]})" "$(bytesToHuman ${ext_freememory_post[0]})" "$(bytesToHuman ${ext_freememory_post[1]})"
+writeToConsole $MSG_LEVEL_MINIMAL $MSG_EXT_DISK_FREE "${ext_freememory_pre[0]}" "${ext_freememory_pre[1]}" "${ext_freememory_post[0]}" "${ext_freememory_post[1]}"
 
 
 
