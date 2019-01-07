27e2d3749ab362ea4e34c492eb8534df2c1ea2f1
PwOSS <pwoss@pwoss.xyz>
2019-01-07 16:09:22 +1300

    Email: MSMTP Support

--- raspiBackup.sh
+++ raspiBackup.sh
@@ -187,8 +187,9 @@ EMAIL_EXTENSION="mail"
 EMAIL_EXTENSION_PROGRAM="mailext"
 EMAIL_MAILX_PROGRAM="mail"
 EMAIL_SSMTP_PROGRAM="ssmtp"
+EMAIL_MSMTP_PROGRAM="msmtp"
 EMAIL_SENDEMAIL_PROGRAM="sendEmail"
-SUPPORTED_EMAIL_PROGRAM_REGEX="^($EMAIL_MAILX_PROGRAM|$EMAIL_SSMTP_PROGRAM|$EMAIL_SENDEMAIL_PROGRAM|$EMAIL_EXTENSION_PROGRAM)$"
+SUPPORTED_EMAIL_PROGRAM_REGEX="^($EMAIL_MAILX_PROGRAM|$EMAIL_SSMTP_PROGRAM|$EMAIL_MSMTP_PROGRAM|$EMAIL_SENDEMAIL_PROGRAM|$EMAIL_EXTENSION_PROGRAM)$"
 SUPPORTED_MAIL_PROGRAMS=$(echo $SUPPORTED_EMAIL_PROGRAM_REGEX | sed 's:^..\(.*\)..$:\1:' | sed 's/|/,/g')
 
 PARTITIONS_TO_BACKUP_ALL="*"
@@ -2295,6 +2296,21 @@ function sendEMail() { # content subject
 						logItem "$EMAIL_PROGRAM: RC: $rc"
 					fi
 					;;
+          $EMAIL_MSMTP_PROGRAM)
+  					if (( $APPEND_LOG )); then
+  						logItem "Sending email with mpack"
+  						echo "$content" > /tmp/$$
+  						mpack -s "$subject" -d /tmp/$$ "$LOG_FILE" "$EMAIL"
+  						rm /tmp/$$ &>/dev/null
+  					else
+  						local sender=${SENDER_EMAIL:-root@$(hostname -f)}
+  						logItem "Sendig email with msmtp"
+  						logItem "echo -e To: $EMAIL\nFrom: $sender\nSubject: $subject\n$content | $EMAIL_PROGRAM $EMAIL"
+  						echo -e "To: $EMAIL\nFrom: $sender\nSubject: $subject\n$content" | "$EMAIL_PROGRAM" -a default "$EMAIL"
+  						rc=$?
+  						logItem "$EMAIL_PROGRAM: RC: $rc"
+  					fi
+  					;;
 				$EMAIL_EXTENSION_PROGRAM)
 					local append=""
 					(( $APPEND_LOG )) && append="$LOG_FILE"
