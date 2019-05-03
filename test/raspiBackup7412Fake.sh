c0736e22a66750d46ff294f9b516f360ca2409e1
framp <framp@linux-tips-and-tricks.de>
2019-05-03 15:07:14 +0200

    Updated commit sha

--- test/raspiBackup7412Fake.sh
+++ test/raspiBackup7412Fake.sh
@@ -38,11 +38,11 @@ MYSELF=${0##*/}
 MYNAME=${MYSELF%.*}
 VERSION="0.1"
 
-set +u;GIT_DATE="$Date: 2019-04-28 22:27:00 +0200$"; set -u
+set +u;GIT_DATE="$Date: 2019-05-03 15:05:59 +0200$"; set -u
 GIT_DATE_ONLY=${GIT_DATE/: /}
 GIT_DATE_ONLY=$(cut -f 2 -d ' ' <<< $GIT_DATE)
 GIT_TIME_ONLY=$(cut -f 3 -d ' ' <<< $GIT_DATE)
-set +u;GIT_COMMIT="$Sha1: 7897617$";set -u
+set +u;GIT_COMMIT="$Sha1: 1fbe96b$";set -u
 GIT_COMMIT_ONLY=$(cut -f 2 -d ' ' <<< $GIT_COMMIT | sed 's/\$//')
 
 GIT_CODEVERSION="$MYSELF $VERSION, $GIT_DATE_ONLY/$GIT_TIME_ONLY - $GIT_COMMIT_ONLY"
