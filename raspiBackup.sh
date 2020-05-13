c0d1b3cd7211fd1710e38b055bb2b9df551868d5
framp <framp@linux-tips-and-tricks.de>
2020-05-13 16:30:56 +0200

    updated sha

--- raspiBackup.sh
+++ raspiBackup.sh
@@ -61,11 +61,11 @@ IS_HOTFIX=$(( ! $(grep -iq hotfix <<< "$VERSION"; echo $?) ))
 MYSELF=${0##*/}
 MYNAME=${MYSELF%.*}
 
-GIT_DATE="$Date: 2020-05-13 15:58:30 +0200$"
+GIT_DATE="$Date: 2020-05-13 16:24:08 +0200$"
 GIT_DATE_ONLY=${GIT_DATE/: /}
 GIT_DATE_ONLY=$(cut -f 2 -d ' ' <<< $GIT_DATE)
 GIT_TIME_ONLY=$(cut -f 3 -d ' ' <<< $GIT_DATE)
-GIT_COMMIT="$Sha1: 151f0f8$"
+GIT_COMMIT="$Sha1: 5eb5d77$"
 GIT_COMMIT_ONLY=$(cut -f 2 -d ' ' <<< $GIT_COMMIT | sed 's/\$//')
 
 GIT_CODEVERSION="$MYSELF $VERSION, $GIT_DATE_ONLY/$GIT_TIME_ONLY - $GIT_COMMIT_ONLY"
