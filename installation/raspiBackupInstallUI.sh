1732cf216ac091312a9d4fde731384a5fb9b2bf2
framps <framp@linux-tips-and-tricks.de>
2021-10-11 22:55:05 +0200

    Fixed version number

--- installation/raspiBackupInstallUI.sh
+++ installation/raspiBackupInstallUI.sh
@@ -38,7 +38,7 @@ fi
 
 MYSELF=${0##*/}
 MYNAME=${MYSELF%.*}
-VERSION="0.4.3.5"							 	# -beta, -hotfix or -dev suffixes possible
+VERSION="0.4.3.6"							 	# -beta, -hotfix or -dev suffixes possible
 
 if [[ (( ${BASH_VERSINFO[0]} < 4 )) || ( (( ${BASH_VERSINFO[0]} == 4 )) && (( ${BASH_VERSINFO[1]} < 3 )) ) ]]; then
 	echo "bash version 0.4.3 or beyond is required by $MYSELF" # nameref feature, declare -n var=$v
