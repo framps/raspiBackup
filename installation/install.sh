0e0fdf59d297f484c9f31ebe78e72ebfe47ec245
GoogleBeEvil <39509734+GoogleBeEvil@users.noreply.github.com>
2021-10-23 03:14:38 +0800

    Update install.sh

--- installation/install.sh
+++ installation/install.sh
@@ -28,7 +28,7 @@
 MYSELF="install"
 VERSION="0.1.1"
 
-URL="https://raspibackup.linux-tips-and-tricks.de"
+URL="https://www.linux-tips-and-tricks.de"
 INSTALLER="raspiBackupInstallUI.sh"
 INSTALLER_DOWNLOAD_URL="$URL/$INSTALLER"
 TO_BE_INSTALLED="raspiBackup.sh"
