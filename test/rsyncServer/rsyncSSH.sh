acfb15f2bd40f1e3a6af3c4a5cefd4d06c2a2a22
framps <framp@linux-tips-and-tricks.de>
2021-11-17 21:32:43 +0100

    Updated rsyncSSH

--- test/rsyncServer/rsyncSSH.sh
+++ test/rsyncServer/rsyncSSH.sh
@@ -2,9 +2,18 @@
 
 # Just some code to get familiar with remote ssh and rsync daemon
 
-source ./.credentials
+source ~/.ssh/rsyncServer.creds
 
-USE_SSH=0
+#SSH_HOST=
+#SSH_USER=
+#SSH_KEY_FILE=
+
+#DAEMON_HOST=
+#DAEMON_TARGET=
+#DAEMON_USER=
+#DAEMON_PASSWORD=
+
+USE_SSH=1
 USE_DAEMON=$((! $USE_SSH ))
 
 # invoke command either local or remote via ssh
@@ -15,7 +24,7 @@ function invoke() { # command [host]
 	if [[ -z $2 ]]; then
 		# $2
 		local host="$(hostname)"
-		ssh "${USER}@$host" "$1"
+		ssh "${SSH_USER}@$host" "$1"
 		rc=$?
 	else
 		ssh "$2" "$1"
@@ -37,28 +46,28 @@ echo -n "@@@ Syning with "
 if (( $USE_SSH )); then
 	echo "SSH ..."
 	# use user key
-	rsync -av --delete -e "ssh -i ${SSH_TARGET[$SSH_KEY]}" $1 ${SSH_TARGET[$SSH_USER]}@${SSH_TARGET[$SSH_HOST]}:/disks/raid1/test
+	rsync -av --delete -e "ssh -i ${SSH_KEY_FILE}" $1 ${SSH_USER}@${SSH_HOST}:/disks/raid1/test
 	checkrc $?
 fi
 
 if (( $USE_DAEMON )); then
 	echo "rsync daemon ..."
 	# use rsync daemon
-	export RSYNC_PASSWORD="${DAEMON_TARGET[$DAEMON_PASSWORD]}"
-	rsync -pv $1 rsync://"${DAEMON_TARGET[$DAEMON_USER]}"@${SSH_TARGET[$SSH_HOST]}:/Test-Backup # points to /disks/raid1/test
+	export RSYNC_PASSWORD="${DAEMON_PASSWORD}"
+	rsync -pv $1 rsync://"${DAEMON_USER}"@${DAEMON_HOST}:/Test-Backup # points to /disks/raid1/test
 	checkrc $?
 fi
 
 echo -e "\n@@@ ls -la"
-invoke "ls -la /disks/raid1/test" ${SSH_TARGET[$SSH_HOST]}
-#ssh ${SSH_TARGET[$SSH_USER]}@${SSH_TARGET[$SSH_HOST]} -i ${SSH_TARGET[$SSH_KEY]} "ls -la /disks/raid1/test"
+invoke "ls -la /disks/raid1/test" ${SSH_HOST}
+#ssh ${SSH_USER}@${SSH_HOST} -i ${SSH_KEY_FILE} "ls -la /disks/raid1/test"
 checkrc $?
 
-#echo -e "\n@@@ sudo rm *"
-#ssh ${SSH_TARGET[$SSH_USER]}@${SSH_TARGET[$SSH_HOST]} -i ${SSH_TARGET[$SSH_KEY]} "sudo rm /disks/raid1/test/*"
-#checkrc $?
+echo -e "\n@@@ sudo rm *"
+ssh ${SSH_USER}@${SSH_HOST} -i ${SSH_KEY_FILE} "sudo rm /disks/raid1/test/*"
+checkrc $?
 
-#echo -e "\n@@@ ls"
-#ssh ${SSH_TARGET[$SSH_USER]}@${SSH_TARGET[$SSH_HOST]} -i ${SSH_TARGET[$SSH_KEY]} "ls /disks/raid1/test"
-#checkrc $?
+echo -e "\n@@@ ls -la"
+ssh ${SSH_USER}@${SSH_HOST} -i ${SSH_KEY_FILE} "ls -la /disks/raid1/test"
+checkrc $?
 
