038b47eeadc738d92bf4deb98547753d6547a096
DesertRider <24532718+DesertRider@users.noreply.github.com>
2021-11-18 08:09:54 -0500

    Update raspiBackup_healthcheck_post.sh

--- extensions/raspiBackup_healthcheck_post.sh
+++ extensions/raspiBackup_healthcheck_post.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# Read our variables on different Healthcheck.io checks
-source /usr/local/etc/healthcheck.conf
-
 if [[ -n $1 ]]; then    # was there a return code ? Should be :-)
     if [[ "$1" == 0 ]]; then
+        # Read our variables on different Healthcheck.io checks
+        source /usr/local/etc/healthcheck.conf
         if [ ! -z  "${raspiBackupPing}"] ; then
             curl -s -m 5 --retry 10 --data-raw "raspiBackup OK" ${raspiBackupPing}
         else
