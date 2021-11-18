926a786d331baa0e08fc875aa639d81170a9d741
DesertRider <24532718+DesertRider@users.noreply.github.com>
2021-11-18 08:09:18 -0500

    Update raspiBackup_healthcheck_pre.sh

--- extensions/raspiBackup_healthcheck_pre.sh
+++ extensions/raspiBackup_healthcheck_pre.sh
@@ -3,7 +3,7 @@
 # Read our variables on different Healthcheck.io checks
 source /usr/local/etc/healthcheck.conf
 
-if [ ! -z  "${raspiBackupPing}"] ; then
+if [ ! -z  "${raspiBackupPing}" ] ; then
     curl -s -m 5 --retry 10 --data-raw "Starting raspiBackup" ${raspiBackupPing}/start
 else
     wall <<< "Extension detected ${0##*/} failed, undefined variable raspiBackupPing"
