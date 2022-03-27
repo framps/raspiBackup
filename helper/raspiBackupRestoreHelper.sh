df1532ff73f132f4ca2254622d40e24f8ee3007e
Franjo G <69915798+franjo-G@users.noreply.github.com>
2022-03-27 13:25:14 +0200

    Language German added (#459)
    * Update raspiBackupRestoreHelper.sh with German messages

--- helper/raspiBackupRestoreHelper.sh
+++ helper/raspiBackupRestoreHelper.sh
@@ -40,21 +40,21 @@ function execution(){
 
     lsblk                      #Output of lsblk to check the drive for restore
     echo ""
-    echo -e "$yellow Please enter the destination drive. e.g. mmcblk0,sda,sdb,sdc.... $normal"
+    echo -e "$yellow $Quest_select_drive $normal"
     read destination
 
     if [[ -e /dev/$destination ]]; then
         echo "OK"
     else
         echo""
-        echo -e "$red Drive not present $normal"
+        echo -e "$red $destination Warn_drive_not_present $normal"
         execution
     fi
 
     if grep -q /$destination /proc/mounts; then
         echo -e "$red -------------------------------------------------------------------------------------------------"
         echo ""
-        echo " At least one partition on the target drive is mounted. Please unmount first".
+        echo " $red $destination $Warn_drive_mounted $normal".
         echo ""
         echo -e " ------------------------------------------------------------------------------------------------- $normal"
       exit 0
@@ -63,9 +63,9 @@ function execution(){
 
     echo -e "$green ----------------------------------------------------------------------------------------------"
     echo ""
-    echo " The backup to restore $backup_path Drive to restore /dev/$destination"
+    echo "$Info_backup_drive $backup_path >>> $destination"
     echo ""
-    echo -e " ------------------------------------------------------------------------------------------------- $no>rmal"
+    echo -e " ------------------------------------------------------------------------------------------------- $normal"
 
     /usr/local/bin/raspiBackup.sh -d /dev/$destination /$backup_path      #Call raspiBackup.sh
     exit 0
@@ -83,7 +83,7 @@ function execution_select(){
     done
 
     echo ""
-    echo -e "$yellow Please enter the number at the end of the desired backup. $normal"
+    echo -e "$yellow $Quest_number_of_backup $normal"
     echo ""
 
     read v
@@ -100,7 +100,7 @@ function execution_select(){
     if [[ -d "$backup_path" ]]; then
 
         execution
-        echo -e "$red Oops The directory does not exist.g? $normal"
+        echo -e "$red $Warn_drive_not_present $normal"
         echo ""
         execution_select
     fi
@@ -112,26 +112,73 @@ function test_digit(){
     if [[ "$1" =~ ^[0-9]+$ ]]; then            # regex: a number has to have
 
         if (( $1 < $2 || $1 > $3 )); then
-            echo -e "$red Invalid number $1 for range $2 - $3 $normal"
+            echo -e "$red $1 $Warn_invalid_number $2 > $3 $normal"
             echo ""
             execution_select
         else
-            echo -e "$green $1 The Backup $4 will be restored $normal"
+            echo -e "$green $Info_restore $backup_path $normal"
             echo""
         fi
 
     else
-        echo -e "$red $1 is no number $normal"
+        echo -e "$red $1 $Warn_no_number $normal"
         echo""
         execution_select
     fi
 
 }
 
+function language(){
+
+        echo ""
+        echo -e "$yellow Please choose your preferred language"
+        echo -e " Bitte waehle deine bevorzugte Sprache"
+        echo ""
+        echo -e " German  = 1"
+        echo -e " English = 2 $normal"
+
+        read lang
+
+    if (( $lang == 1 )); then
+	Quest_last_backup="Soll das letzte Backup restored werden? y/N"
+	Quest_select_drive="Bitte waehle das Ziellaufwerk z.B. mmcblk0,sda,sdb,sdc...."
+	Warn_drive_not_present="Das Ziellaufwerk existiert nicht"
+	Warn_drive_mounted="Mindestens eine Partition ist gemountet. Bitte erst aushaengen."
+	Info_backup_drive="Folgendes Backup wird restored "
+	Quest_number_of_backup="Bitte gebe die hinter dem gewuenschten Backups stehende Zahl ein. "
+	Warn_no_dir="Oops Das Verzeichnis existier nicht."
+	Warn_invalid_number="Die eingegebene Zahl ist ungueltig. Nur Zahelen im Bereich von "
+	Info_restore="Das folgende Backup wird zurueckgespielt "
+	Warn_no_number="Das ist keine Zahl "
+	Warn_false_number="Falsche Eingabe Bitte nur 1 oder 2 eingeben "
+
+    elif (( $lang == 2 )); then
+	Quest_last_backup="Should the last backup be restored? y/N "
+	Quest_select_drive="Please enter the destination drive. e.g. mmcblk0,sda,sdb,sdc.... "
+	Warn_drive_not_present="Drive is not present "
+	Warn_drive_mounted="At least one partition on the target drive is mounted. Please unmount first ".
+	Info_backup_drive="$The backup to restore $backup_path Drive to restore "
+	Quest_number_of_backup="$Please enter the number at the end of the desired backup. "
+	Warn_no_dir="Oops The directory does not exist "
+	Warn_invalid_number="Invalid number Please enter only numbers in range "
+	Info_restore="The following Backup will be restored "
+	Warn_no_number="That is no number "
+	Warn_false_number="Please enter 1 or 2 "
+
+    else
+	echo -e "$red False input. Please enter only 1 or 2"
+	echo -e " Falsche Eingabe. Bitte nur 1 oder 2 eingeben $normal"
+	language
+    fi
+}
+
 if (( $UID != 0 )); then
     echo -e "$red Script has to be called as root or with sudo $normal"
-    exit
+    echo -e "$red Das Script muss als root oder mit sudo aufgerufen werden $normal"
+	exit
 fi
+
+	language
 
 source $FILE
 backupdir=$DEFAULT_BACKUPPATH
@@ -148,7 +195,7 @@ elif [[ $1 == "--select" ]]; then
 
 else
     echo ""
-    echo -e "$yellow Should the last backup be restored? y/N $normal"
+    echo -e "$yellow $Quest_last_backup $normal"
     read answer
 fi
 
