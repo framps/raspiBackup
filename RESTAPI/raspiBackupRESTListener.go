f3897f538e353d29af8d4fcf09cda6139d4f5e8c
framps <framp@linux-tips-and-tricks.de>
2022-07-25 20:13:58 +0200

    Use modules instead of vendoring and glide now

--- RESTAPI/raspiBackupRESTListener.go
+++ RESTAPI/raspiBackupRESTListener.go
@@ -21,7 +21,24 @@ GET /v1/raspiBackup/query?value=n - return a payload telling whether a query par
 GET /v1/raspiBackup/param/:param - return a payload telling whether param was passed and what's the value
 GET / - returns a nice welcome html page
 
-(c) 2017-2018 - framp at linux-tips-and-tricks dot de
+#######################################################################################################################
+ #
+ #    Copyright (c) 2017-2018 framp at linux-tips-and-tricks dot de
+ #
+ #    This program is free software: you can redistribute it and/or modify
+ #    it under the terms of the GNU General Public License as published by
+ #    the Free Software Foundation, either version 3 of the License, or
+ #    (at your option) any later version.
+ #
+ #    This program is distributed in the hope that it will be useful,
+ #    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ #    GNU General Public License for more details.
+ #
+ #    You should have received a copy of the GNU General Public License
+ #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ #
+ #######################################################################################################################
 
 */
 
