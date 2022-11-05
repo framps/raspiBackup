d3e687a1640f2d7ddded774a2c2b90150b44b006
framps <framp@linux-tips-and-tricks.de>
2022-11-05 19:05:24 +0100

    s

--- scripts/raspiBackupFromGitInvocation.sh
+++ scripts/raspiBackupFromGitInvocation.sh
@@ -105,9 +105,9 @@ if [[ -z $date ]]; then
 fi
 
 shaShort=${sha:0:7}
-sed -E -i "s/$SHA/$SHA ${shaShort}/" ./raspiBackup.sh
+sed -i "s/\$SHA/\$SHA ${shaShort}/" ./raspiBackup.sh
 dateShort="${date:0:10} ${date:11}"
-sed -E -i "s/$DATE/$DATE ${dateShort}/" ./raspiBackup.sh
+sed -i "s/\$DATE/\$DATE ${dateShort}/" ./raspiBackup.sh
 
 rm -f $jsonFile
 
