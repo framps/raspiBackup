103ef9428c809d681eb25808cb8b5cbc114d7a96
framps <framp@linux-tips-and-tricks.de>
2022-11-05 19:27:48 +0100

    s

--- scripts/raspiBackupFromGitInvocation.sh
+++ scripts/raspiBackupFromGitInvocation.sh
@@ -45,8 +45,8 @@ if [[ "$1" == "-h" || "$1" == "--help" || "$1" == "-?" || "$1" == "?" ]]; then
 	exit 1
 fi
 
-SHA="XFxcXFxcJFNoYTE6Cg=="
-DATE="XFxcXFxcJERhdGU6Cg=="
+SHA="XFxcXCRTaGExOgo="
+DATE="XFxcXCREYXRlOgo=="
 
 SHA="$(base64 -d <<< "$SHA")"
 DATE="$(base64 -d <<< "$DATE")"
