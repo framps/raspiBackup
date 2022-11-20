f3f8ce8efe2fc6e2d6ec90008d7f0b8a5395fed7
framps <framp@linux-tips-and-tricks.de>
2022-11-20 18:20:56 +0100

    Updated version

--- raspiBackup.sh
+++ raspiBackup.sh
@@ -43,10 +43,10 @@ fi
 MYSELF="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"					# use linked script name if the link is used
 MYNAME=${MYSELF%.*}
 
-VERSION="0.6.8-beta-slack"											# -beta, -hotfix or -dev suffixes possible
+VERSION="0.6.8-beta"										# -beta, -hotfix or -dev suffixes possible
 VERSION_SCRIPT_CONFIG="0.1.7"								# required config version for script
 
-VERSION_VARNAME="VERSION"										# has to match above var names
+VERSION_VARNAME="VERSION"									# has to match above var names
 VERSION_CONFIG_VARNAME="VERSION_.*CONF.*"					# used to lookup VERSION_CONFIG in config files
 
 [ $(kill -l | grep -c SIG) -eq 0 ] && printf "\n\033[1;35m Don't call script with leading \"sh\"! \033[m\n\n"  >&2 && exit 255
