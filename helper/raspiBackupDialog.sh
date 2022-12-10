fb8e631a9754a12b1edb2e392f5b74334fa35b9e
Franjo G <69915798+franjo-G@users.noreply.github.com>
2022-12-10 14:48:40 +0100

    Message added
    Added a Message "raspiBackup will be started now"

--- helper/raspiBackupDialog.sh
+++ helper/raspiBackupDialog.sh
@@ -108,8 +108,11 @@ function backup_add_part_and_comment(){
 	if [[ ${Quest_comment,,} =~ [yj] ]]; then
 		echo -e "$yellow $Quest_comment_text \n $normal"
 		read Quest_comment_text
+		
+		echo -e "$Info_start \n"
 		/usr/local/bin/raspiBackup.sh -M "$Quest_comment_text" -P -T "1 2 $partitions"
 	else
+		echo -e "$Info_start \n"
 		/usr/local/bin/raspiBackup.sh -P -T "1 2 $partitions"
 	fi
 }
@@ -121,8 +124,11 @@ function backup_add_comment(){
 	if [[ ${Quest_comment,,} =~ [yj] ]]; then
 		echo -e "$yellow $Quest_comment_text \n $normal"
 		read Quest_comment_text
+		echo -e "$Info_start \n"
+		
 		/usr/local/bin/raspiBackup.sh -M "$Quest_comment_text" "$1"
 	else
+		echo -e "$Info_start \n"
 		/usr/local/bin/raspiBackup.sh "$1"
 	fi
 }
@@ -152,7 +158,7 @@ function execution(){
 	fi
 
 	echo -e "$green $Info_backup_drive \n $backup_path \n >>> $destination \n $normal"
-
+	echo -e "$Info_start \n"
 	/usr/local/bin/raspiBackup.sh -d /dev/$destination /$backup_path      #Call raspiBackup.sh
 	exit 0
 }
@@ -302,6 +308,7 @@ function language(){
 		Info_already_mounted="Das Backupverzeichnis ist bereits gemountet. Es wird im Anschluss nicht ausgehängt."
 		Info_is_mounted="Das Backupverzeichnis wurde gemountet. Es wird im Anschluss ausgehängt"
 		Info_not_mounted="Das Backupverzeichnis konnte nicht gemountet werden"
+		Info_start="raspiBackup wird jetzt gestartet"
 
 	elif (( $lang == 2 )); then
 		Quest_last_backup="Should the last backup be restored? y/N "
@@ -328,7 +335,7 @@ function language(){
 		Info_already_mounted="The backup directory is already mounted. It will not be unmounted afterwards"
 		Info_is_mounted="The backup directory was mounted. It will be unmounted afterwards"
 		Info_not_mounted="The backup directory could not be mounted"
-
+		Info_start="raspiBackup will be started now"
 	else
 		echo -e "$red False input. Please enter only 1 or 2"
 		echo -e " Falsche Eingabe. Bitte nur 1 oder 2 eingeben $normal"
