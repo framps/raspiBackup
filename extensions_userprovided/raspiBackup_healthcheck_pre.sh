cd94431fb75a2748097fd1edaa1b7280d921b350
p1r473 <subwayjared@gmail.com>
2023-03-01 10:27:29 -0500

    Update raspiBackup_healthcheck_pre.sh

--- extensions_userprovided/raspiBackup_healthcheck_pre.sh
+++ extensions_userprovided/raspiBackup_healthcheck_pre.sh
@@ -5,6 +5,7 @@ source /usr/local/etc/healthcheck.conf
 
 if [ ! -z  "${raspiBackupPing}" ] ; then
     curl -s -m 5 --retry 10 --data-raw "Starting raspiBackup" ${raspiBackupPing}/start
+    echo ""
 else
     wall <<< "Extension detected ${0##*/} failed, undefined variable raspiBackupPing"
     return 1
