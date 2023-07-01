013de5a825b909ddeb4514497b33887a3abafc7f
framps <framp@linux-tips-and-tricks.de>
2023-07-01 18:15:40 +0200

    Fixed #562

--- raspiBackup.sh
+++ raspiBackup.sh
@@ -48,6 +48,7 @@ VERSION_SCRIPT_CONFIG="0.1.7"								# required config version for script
 
 VERSION_VARNAME="VERSION"									# has to match above var names
 VERSION_CONFIG_VARNAME="VERSION_.*CONF.*"					# used to lookup VERSION_CONFIG in config files
+COMMIT_VARNAME="GIT_COMMIT_ONLY"							# used to loockup the SHA
 
 [ $(kill -l | grep -c SIG) -eq 0 ] && printf "\n\033[1;35m Don't call script with leading \"sh\"! \033[m\n\n"  >&2 && exit 255
 [ -z "${BASH_VERSINFO[0]}" ] && printf "\n\033[1;35m Make sure you're using \"bash\"! \033[m\n\n" >&2 && exit 255
@@ -1905,7 +1906,12 @@ MSG_DE[$MSG_IMG_BOOT_FSCHECK_FAILED]="RBK0290E: Bootpartitioncheck endet fehlerh
 MSG_IMG_BOOT_CHECK_STARTED=291
 MSG_EN[$MSG_IMG_BOOT_CHECK_STARTED]="RBK0291I: Bootpartition check started."
 MSG_DE[$MSG_IMG_BOOT_CHECK_STARTED]="RBK0291I: Bootpartitionscheck gestartet."
-
+MSG_MINOR_UPDATE=292
+MSG_EN[$MSG_MINOR_UPDATE]="RBK0292I: There is a minor update of $MYNAME $VERSION available. Do you want to update $MYNAME?"
+MSG_DE[$MSG_MINOR_UPDATE]="RBK0292I: Es gibt einen kleinen Update von $MYNAME $VERSION. Soll $MYNAME updated werden?"
+MSG_SCRIPT_MINOR_UPDATE_OK=293
+MSG_EN[$MSG_SCRIPT_MINOR_UPDATE_OK]="RBK0293I: Minor update of %s successfull"
+MSG_DE[$MSG_SCRIPT_MINOR_UPDATE_OK]="RBK0293I: Kleiner Update von %s erfolgreich."
 declare -A MSG_HEADER=( ['I']="---" ['W']="!!!" ['E']="???" )
 
 # setup trap function
@@ -3496,6 +3502,25 @@ function updateScript() {
 			if askYesNo; then
 				updateNow=1
 			fi
+		else
+			local dlHttpCode dlRC
+			local tmpFile="/tmp/${MYSELF}~"
+			dlHttpCode="$(downloadFile "$DOWNLOAD_URL" "$tmpFile")"
+			dlRC=$?
+			if (( $dlRC != 0 )); then
+				logItem "Ignoring download error $dlRC"
+			else
+				local latestSHA
+				latestSHA="$(extractVersionFromFile "$tmpFile" "$COMMIT_VARNAME")"
+				rm $tmpFile
+				if [[ $latestSHA != $GIT_COMMIT_ONLY ]]; then
+					writeToConsole $MSG_LEVEL_MINIMAL $MSG_MINOR_UPDATE
+					if askYesNo; then
+						updateNow=1
+						minorUpdate=1
+					fi
+				fi
+			fi
 		fi
 
 		if (( $updateNow )); then
@@ -3515,7 +3540,11 @@ function updateScript() {
 			mv $MYSELF~ $SCRIPT_DIR/$MYSELF
 			chown --reference=$newName $SCRIPT_DIR/$MYSELF
 			chmod --reference=$newName $SCRIPT_DIR/$MYSELF
-			writeToConsole $MSG_LEVEL_MINIMAL $MSG_SCRIPT_UPDATE_OK "$SCRIPT_DIR/$MYSELF" "$oldVersion" "$newVersion" "$newName"
+			if (( ! $minorUpdate )); then
+				writeToConsole $MSG_LEVEL_MINIMAL $MSG_SCRIPT_UPDATE_OK "$SCRIPT_DIR/$MYSELF" "$oldVersion" "$newVersion" "$newName"
+			else
+				writeToConsole $MSG_LEVEL_MINIMAL $MSG_SCRIPT_MINOR_UPDATE_OK "$VERSION"
+			fi
 			# refresh version information from updated script
 			local properties="$(grep "^VERSION=" "$SCRIPT_DIR/$MYSELF" 2>/dev/null)"
 			[[ $properties =~ $PROPERTY_REGEX ]] && VERSION=${BASH_REMATCH[1]}
