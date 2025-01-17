14a6e28ae6003ce3f473a3c64281fa864a74bb4f
framps <framp@linux-tips-and-tricks.de>
2025-01-17 15:51:01 +0100

    Updated pdevice list

--- raspiBackup.sh
+++ raspiBackup.sh
@@ -8105,7 +8105,7 @@ function listDeviceInfo() { # device (/dev/sda)
 
 	logEntry "$1"
 	local result
-	result="$(IFS='' lsblk $1 --tree -o NAME,SIZE,FSTYPE,LABEL,UUID,PARTUUID)"
+	result="$(IFS='' lsblk $1 --tree -o NAME,SIZE,FSTYPE,LABEL)"
 	echo "$result"
 	logExit
 }
