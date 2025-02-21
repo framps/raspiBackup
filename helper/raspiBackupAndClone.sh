c27267d2a4faa341d74810ee2ed4c4753d91b50b
framp <framp@linux-tips-and-tricks.de>
2025-02-21 15:32:21 +0100

    Updated raspiBackupAndClone

--- helper/raspiBackupAndClone.sh
+++ helper/raspiBackupAndClone.sh
@@ -4,13 +4,16 @@
 #
 #  Sample script which calls raspiBackup.sh to create a backup and restores the backup to a device (e.g. SD card or USB disk) afterwards
 #
+#  NOTE: Backup can be synced to speed up the restore when rsync partition oriented mode and new option -00 introduced in raspiBackup release 0.7 is used
+#        See comments below marked with ===>  
+#
 #  Visit http://www.linux-tips-and-tricks.de/raspiBackup for details about raspiBackup
 #
 #  NOTE: This is sample code how to extend functionality of raspiBackup and is provided as is with no support.
 #
 #######################################################################################################################
 #
-#   Copyright (c) 2024 framp at linux-tips-and-tricks dot de
+#   Copyright (c) 2024-2025 framp at linux-tips-and-tricks dot de
 #
 #   This program is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
@@ -30,36 +33,53 @@
 set -euf -o pipefail
 
 MYSELF=${0##*/}
-MYNAME=${MYSELF%.*}
-VERSION="0.1"
+VERSION="v0.2"
 
-set +u;GIT_DATE="$Date$"; set -u
+set +u
+#shellcheck disable=SC2154
+#(warning): Date is referenced but not assigned
+GIT_DATE="$Date$"
+set -u
 GIT_DATE_ONLY=${GIT_DATE/: /}
-GIT_DATE_ONLY=$(cut -f 2 -d ' ' <<< $GIT_DATE)
-GIT_TIME_ONLY=$(cut -f 3 -d ' ' <<< $GIT_DATE)
-set +u;GIT_COMMIT="$Sha1$";set -u
-GIT_COMMIT_ONLY=$(cut -f 2 -d ' ' <<< $GIT_COMMIT | sed 's/\$//')
+GIT_DATE_ONLY=$(cut -f 2 -d ' ' <<< "$GIT_DATE")
+GIT_TIME_ONLY=$(cut -f 3 -d ' ' <<< "$GIT_DATE")
+set +u
+#shellcheck disable=SC2154
+#(warning): Sha1 is referenced but not assigned
+GIT_COMMIT="$Sha1$"
+set -u
+GIT_COMMIT_ONLY=$(cut -f 2 -d ' ' <<< "$GIT_COMMIT" | sed 's/\$//')
 
 GIT_CODEVERSION="$MYSELF $VERSION, $GIT_DATE_ONLY/$GIT_TIME_ONLY - $GIT_COMMIT_ONLY"
 
-if (( $UID != 0 )); then
+if (( UID != 0 )); then
       echo "--- Call me as root or with sudo"
       exit 1
 fi
+
+echo "$GIT_CODEVERSION"
 
 # add pathes if not already set (usually not set in crontab)
 
 if [[ -e /bin/grep ]]; then
    PATHES="/bin /sbin /usr/bin /usr/sbin /usr/local/bin /usr/local/sbin"
    for p in $PATHES; do
-      if ! /bin/grep -E -q "[\^:]$p[:$]" <<< $PATH; then
-         [[ -z $PATH ]] && export PATH=$p || export PATH="$p:$PATH"
+      # shellcheck disable=SC1087
+      # (error): Use braces when expanding arrays, e.g. ${array[idx]} (or ${var}[.. to quiet).
+      if ! /bin/grep -E -q "[\^:]$p[:$]" <<< "$PATH"; then
+         if [[ -z $PATH ]]; then 
+            export PATH=$p
+         else
+            export PATH="$p:$PATH"
+         fi
       fi
    done
 fi
 
 function readVars() {
    if [[ -f /tmp/raspiBackup.vars ]]; then
+      # shellcheck disable=SC1091
+      # (info) Not following: /usr/local/etc/raspiBackup.conf was not specified as input (see shellcheck -x).
       source /tmp/raspiBackup.vars                 # retrieve some variables from raspiBackup for further processing
 # now following variables are available for further backup processing
 # BACKUP_TARGETDIR refers to the backupdirectory just created
@@ -94,22 +114,33 @@ fi
 echo "--- Creating backup and restore backup afterwards to $CLONE_DEVICE ..."
 
 # create backup
+# ===>
+# ===> Use option -t rsync, -P, -T to speed up the restore with new raspiBackup release 0.7 option -00
+# ===>
 raspiBackup.sh
+#shellcheck disable= SC2181
+#(style): Check exit code directly with e.g. 'if ! mycmd;', not indirectly with $?.
 if (( ! $? )); then
    readVars
    # BACKUP_TARGETDIR now refers to the just created backup
    # now restore backup to device
    f=$(mktemp)
-   echo "DEFAULT_YES_NO_RESTORE_DEVICE=$CLONE_DEVICE" > $f
+   echo "DEFAULT_YES_NO_RESTORE_DEVICE=$CLONE_DEVICE" > "$f"
+   # shellcheck disable=SC1091
+   # (info) Not following: /usr/local/etc/raspiBackup.conf was not specified as input (see shellcheck -x).
    source /usr/local/etc/raspiBackup.conf
    mounted=0
    if ! mount | grep "$DEFAULT_BACKUPPATH"; then
-		sudo mount "$DEFAULT_BACKUPPATH"
-		mounted=1
+        sudo mount "$DEFAULT_BACKUPPATH"
+        mounted=1
    fi
-   raspiBackup.sh -Y -d $CLONE_DEVICE -f $f "$BACKUP_TARGETDIR"
-   rm $f
-   if (( $mounted )); then
-		sudo umount "$DEFAULT_BACKUPPATH"
-   fi	
+   # restore backup to clone device or sync clone device with backup
+   # ===>
+   # ===> Use Option -T and -00 to speed up the restore
+   # ===>
+   raspiBackup.sh -Y -d "$CLONE_DEVICE" -f "$f" "$BACKUP_TARGETDIR"
+   rm "$f"
+   if (( mounted )); then
+        sudo umount "$DEFAULT_BACKUPPATH"
+   fi   
 fi
