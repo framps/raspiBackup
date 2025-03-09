65467a2ae74f57772ae3ac22be7bbcc550ea720d
framps <framp@linux-tips-and-tricks.de>
2025-03-09 21:21:44 +0100

    Save

--- scripts/raspiBackupMigrateOldDirnames.sh
+++ scripts/raspiBackupMigrateOldDirnames.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+#######################################################################################################################
+#
+#    Copyright (c) 2025 framp at linux-tips-and-tricks dot de
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#######################################################################################################################
+
 set -eou pipefail
 
 declare -r PS4='|${LINENO}> \011${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
