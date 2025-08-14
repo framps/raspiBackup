758f7b84d36b4cb489aa13a5f9df912cfc96fd3f
framps <framp@linux-tips-and-tricks.de>
2025-08-14 21:12:33 +0200

    Removed useless error message

--- raspiBackup.sh
+++ raspiBackup.sh
@@ -7034,64 +7034,67 @@ function reportOldBackups() {
 		(( $keepOverwrite != 0 )) && keepBackups=$keepOverwrite
 	fi
 
-	if ! pushd "$BACKUPPATH" &>>"$LOG_FILE"; then
-		assertionFailed $LINENO "push to $BACKUPPATH failed"
-	fi
+	if ls -d ${HOSTNAME}-${BACKUPTYPE}-backup-* &>/dev/null; then 
 
-	# Double quote to prevent globbing and word splitting.
-	# Don't use ls | grep. Use a glob or a for loop with a condition to allow non-alphanumeric filenames.
-	#shellcheck disable=SC2010,SC2086
-	tobeListedOldBackups=$(ls -d ${HOSTNAME}-${BACKUPTYPE}-backup-* 2>>"$LOG_FILE")
-
-	if [[ -n $tobeListedOldBackups ]]; then
-
-		local report_counter_file="$VAR_LIB_DIRECTORY/$REPORT_COUNTER_FILE"
-
-		# create directory to save counter
-		if [[ ! -d "$VAR_LIB_DIRECTORY" ]]; then
-			mkdir -p "$VAR_LIB_DIRECTORY"
+		if ! pushd "$BACKUPPATH" &>>"$LOG_FILE"; then
+			assertionFailed $LINENO "push to $BACKUPPATH failed"
 		fi
 
-		# initialize counter
-		if [[ ! -e "$report_counter_file" ]]; then
-			echo "$OLD_REMINDER_REPEAT" > "$report_counter_file"
-		fi
+		# Double quote to prevent globbing and word splitting.
+		# Don't use ls | grep. Use a glob or a for loop with a condition to allow non-alphanumeric filenames.
+		#shellcheck disable=SC2010,SC2086
+		tobeListedOldBackups=$(ls -d ${HOSTNAME}-${BACKUPTYPE}-backup-* 2>>"$LOG_FILE")
 
-		# retrieve counter
-		local rf
-		rf=$(<$report_counter_file)
-		if [[ -z "${rf}" ]]; then				# counter file exists but is empty
-			echo "$OLD_REMINDER_REPEAT" > "$report_counter_file"
-			return
-		fi
-		rf=$(<$report_counter_file)
+		if [[ -n $tobeListedOldBackups ]]; then
 
-		# only print report if counter says so
-		if (( $rf > 0 )); then
+			local report_counter_file="$VAR_LIB_DIRECTORY/$REPORT_COUNTER_FILE"
 
-			# update counter, but only if not in FAKE mode
-			local rfn=$(( ${rf} - 1 ))
-			if (( ! $FAKE )); then
-				echo "${rfn}" > "$report_counter_file"
+			# create directory to save counter
+			if [[ ! -d "$VAR_LIB_DIRECTORY" ]]; then
+				mkdir -p "$VAR_LIB_DIRECTORY"
 			fi
 
-			writeToConsole $MSG_LEVEL_MINIMAL $MSG_GENERIC_WARNING "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-			writeToConsole $MSG_LEVEL_MINIMAL $MSG_BACKUP_NAMING_CHANGE "0.7"
-			writeToConsole $MSG_LEVEL_MINIMAL $MSG_OLD_NAME_BACKUPS_FOUND
-			echo "$tobeListedOldBackups" | while read -r dir_to_list; do
-				[[ -n $dir_to_list ]] && writeToConsole $MSG_LEVEL_MINIMAL $MSG_GENERIC_WARNING "  - $BACKUPTARGET_ROOT/${dir_to_list}"
-			done
-			writeToConsole $MSG_LEVEL_MINIMAL $MSG_OLD_NAME_BACKUPS_HANDLING_INFO
-			writeToConsole $MSG_LEVEL_MINIMAL $MSG_OLD_NAME_BACKUPS_COUNTER_INFO "${rfn}"
+			# initialize counter
+			if [[ ! -e "$report_counter_file" ]]; then
+				echo "$OLD_REMINDER_REPEAT" > "$report_counter_file"
+			fi
 
-			writeToConsole $MSG_LEVEL_MINIMAL $MSG_GENERIC_WARNING "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+			# retrieve counter
+			local rf
+			rf=$(<$report_counter_file)
+			if [[ -z "${rf}" ]]; then				# counter file exists but is empty
+				echo "$OLD_REMINDER_REPEAT" > "$report_counter_file"
+				return
+			fi
+			rf=$(<$report_counter_file)
 
+			# only print report if counter says so
+			if (( $rf > 0 )); then
+
+				# update counter, but only if not in FAKE mode
+				local rfn=$(( ${rf} - 1 ))
+				if (( ! $FAKE )); then
+					echo "${rfn}" > "$report_counter_file"
+				fi
+
+				writeToConsole $MSG_LEVEL_MINIMAL $MSG_GENERIC_WARNING "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+				writeToConsole $MSG_LEVEL_MINIMAL $MSG_BACKUP_NAMING_CHANGE "0.7"
+				writeToConsole $MSG_LEVEL_MINIMAL $MSG_OLD_NAME_BACKUPS_FOUND
+				echo "$tobeListedOldBackups" | while read -r dir_to_list; do
+					[[ -n $dir_to_list ]] && writeToConsole $MSG_LEVEL_MINIMAL $MSG_GENERIC_WARNING "  - $BACKUPTARGET_ROOT/${dir_to_list}"
+				done
+				writeToConsole $MSG_LEVEL_MINIMAL $MSG_OLD_NAME_BACKUPS_HANDLING_INFO
+				writeToConsole $MSG_LEVEL_MINIMAL $MSG_OLD_NAME_BACKUPS_COUNTER_INFO "${rfn}"
+
+				writeToConsole $MSG_LEVEL_MINIMAL $MSG_GENERIC_WARNING "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+
+			fi
 		fi
-	fi
 
-	if ! popd &>>"$LOG_FILE"; then
-		assertionFailed $LINENO "pop failed"
-	fi
+		if ! popd &>>"$LOG_FILE"; then
+			assertionFailed $LINENO "pop failed"
+		fi
+	
 }
 
 function backup() {
