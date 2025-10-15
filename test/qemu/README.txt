1c9c95aa489d3bdc10aa52278c4adcb48b55006d
framp <framp@linux-tips-and-tricks.de>
2025-10-15 15:03:51 +0200

    init updated

--- test/qemu/README.txt
+++ test/qemu/README.txt
@@ -3,11 +3,5 @@ The qemu setup was done accoriding the instructions available on https://crycode
 Steps to build regressionenvironment:
 1) Execute build-qemu-kernel.sh
 2) Execute rpi-emu-create-image.sh
-3) Execute ../rpi-emu-start.sh Bookworm.qcow2
-4) Login into image and wait until new image finished all initial setup steps
-5) cd ..
-6) Adapt env.defs to you environment
-7) scp your pub user ssh credential to the image user pi
-8) scp your root user ssh credential to the image user pi
-9) copy the pi authorized_keys to root on the image
+3) Execute rpi-emu-init-image.sh
 
--- test/qemu/rpi-emu-init-image.sh
+++ test/qemu/rpi-emu-init-image.sh
@@ -12,17 +12,26 @@ if ! ping -c 1 $DEPLOYED_IP; then
                	sleep 3
 		echo -n "."
         done
-
+	echo
 	echo "Waiting for ssh to come up ..."
 	while ! nc -zv $DEPLOYED_IP 22 &>/dev/null; do
 		sleep 3
 		echo -n "."
 	done
+	echo
 fi
 
-echo "Copy pub key"
+sudo ssh-keygen -f '/root/.ssh/known_hosts' -R '192.168.0.191'
+echo "Copy my pub key to pi"
 ssh-copy-id pi@$DEPLOYED_IP
-sudo scp /root/.ssh/id_rsa.pub pi@$DEPLOYED_IP:~
-echo "Copy  authorized_keys"
+echo "Copy my root pub key to pi"
+sudo cp /root/.ssh/id_rsa.pub .
+scp id_rsa.pub pi@$DEPLOYED_IP:~
+echo "Updating authorized_keys of pi"
+ssh pi@$DEPLOYED_IP "cat /home/pi/id_rsa.pub >> authorized_keys"
+ssh pi@$DEPLOYED_IP "rm /home/pi/id_rsa.pub"
+echo "Updating authorized_keys of root on pi"
 ssh pi@$DEPLOYED_IP "sudo cp /home/pi/.ssh/authorized_keys /root/.ssh/authorized_keys"
-
+sudo rm id_rsa.pub
+#echo "Shutting down $DEPLOYED_IP
+#ssh pi@$DEPLOYED_IP "sudo shutdown -h now"
