e316813f1816339fb2d1e7887ac3aae04c17bfc9
framp <framp@linux-tips-and-tricks.de>
2025-10-25 17:09:21 +0200

    Added cpu model log

--- raspiBackup.sh
+++ raspiBackup.sh
@@ -2254,6 +2254,7 @@ function logSystem() {
 	[[ -f /etc/os-release ]] &&	logCommand "cat /etc/os-release"
 	[[ -f /etc/debian_version ]] &&	logCommand "cat /etc/debian_version"
 	[[ -f /etc/fstab ]] &&	logCommand "cat /etc/fstab"
+	[[ -f /proc/cpuinfo ]] && logCommand "cat /proc/cpuinfo | grep -i model"
 	logCommand "locale"
 	logExit
 }
