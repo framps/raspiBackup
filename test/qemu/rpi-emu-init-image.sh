fe150702c3f354ff7061bca93245dc130bd7af4f
framp <framp@linux-tips-and-tricks.de>
2025-11-01 21:28:49 +0100

    Added var for id key

--- test/qemu/rpi-emu-init-image.sh
+++ test/qemu/rpi-emu-init-image.sh
@@ -32,13 +32,13 @@ sudo ssh-keygen -f '/root/.ssh/known_hosts' -R '192.168.0.191'
 echo "Copy my pub key to pi"
 ssh-copy-id pi@$DEPLOYED_IP
 echo "Copy my root pub key to pi"
-sudo cp /root/.ssh/id_rsa.pub .
-scp id_rsa.pub pi@$DEPLOYED_IP:~
+sudo cp /root/.ssh/id_$ID_SUFFIX.pub .
+scp id_$ID_SUFFIX.pub pi@$DEPLOYED_IP:~
 echo "Updating authorized_keys of pi"
-ssh pi@$DEPLOYED_IP "cat /home/pi/id_rsa.pub >> authorized_keys"
-ssh pi@$DEPLOYED_IP "rm /home/pi/id_rsa.pub"
+ssh pi@$DEPLOYED_IP "cat /home/pi/id_$ID_SUFFIX.pub >> authorized_keys"
+ssh pi@$DEPLOYED_IP "rm /home/pi/id_$ID_SUFFIX.pub"
 echo "Updating authorized_keys of root on pi"
 ssh pi@$DEPLOYED_IP "sudo cp /home/pi/.ssh/authorized_keys /root/.ssh/authorized_keys"
-sudo rm id_rsa.pub
+sudo rm id_$ID_SUFFIX.pub
 echo "Shutting down $DEPLOYED_IP"
 ssh pi@$DEPLOYED_IP "sudo shutdown -h now"
