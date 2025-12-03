f5e4314661fc166deea4b6daee904fb437e6c68f
framp <framp@linux-tips-and-tricks.de>
2025-12-03 18:09:14 +0100

    Updated constants

--- test/constants.sh
+++ test/constants.sh
@@ -33,7 +33,7 @@ BOOTMODE_TAR="-B+"
 
 MOUNT_HOST=192.168.0.194	# host used to mount remote backup dir
 DEPLOYED_IP=192.168.0.191	# IP of simulated Raspberry
-EXPORT_DIR="/nfs"		# backup directory on host which holds the backups and is exported
+EXPORT_DIR="/nfs/backup"	# backup directory on host which holds the backups and is exported
 BACKUP_DIR="regression"		# -> /nfs/regression_N and /nfs/regression_P
 
 LOG_REGRESSION="raspiBackupRegression.log"
