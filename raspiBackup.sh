170c3291fe216da5cf105c8e8e4c450f315e16c7
framp <framp@linux-tips-and-tricks.de>
2026-03-01 20:45:26 +0100

    Tolerate tar

--- raspiBackup.sh
+++ raspiBackup.sh
@@ -9006,11 +9006,15 @@ function getCompressionTool() { # backupfilename
 		compressionTool="-I ${TAR_COMPRESSION_TOOLS_SUPPORTED[$i]}"
 		logItem "Compressiontool $compressionTool used"
 	else
-		assertionFailed $LINENO "Incorrect compressiontool $compressionTool"
+		if [[ "$extension" == ".tar" ]]; then
+			compressionTool=""
+			logItem "No compressiontool used"
+		else
+			assertionFailed $LINENO "Incorrect compressiontool $compressionTool"
+		fi
 	fi
 	echo "$compressionTool"
-	logExit "$compressionTool"
-
+	logExit "- $compressionTool -"
 	}
 
 function restorePartitionBasedPartition() { # restorefile
