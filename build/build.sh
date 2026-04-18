24a5c685e803970d21ded440a8d35e5213a3c8c9
framp <framp@linux-tips-and-tricks.de>
2026-04-18 22:24:39 +0200

    upd

--- build/build.sh
+++ build/build.sh
@@ -11,15 +11,10 @@ export DEBSIGN_KEYID=8517A08D66D5D9B6
 export DEBEMAIL=framp@linux-tips-and-tricks.de
 # Export public key
 # gpg --armor --export 8517A08D66D5D9B6 > 8517A08D66D5D9B6.pub
-show "Cleanup installation"
-sudo apt remove -y raspibackup rsync
 show "Build package"
 dpkg-deb --root-owner-group --build raspiBackup_0.7.2
 show "Sign package"
 gpg --yes --detach-sign raspiBackup_0.7.2.deb
-# Retrieve key from github
-# curl https://github.com/framps.gpg > framps.sig
-#
 show "Show files which will be installed"
 dpkg-deb -c raspiBackup_0.7.2.deb
 
--- build/install.sh
+++ build/install.sh
@@ -5,8 +5,19 @@ function show() {
 	echo "@@@ $@ ..."
 	echo "==============================="
 }
+show "Cleanup installation"
+sudo apt remove -y raspibackup rsync
+# Retrieve key from github
+curl https://github.com/framps.gpg > framps.sig
+#
 show "Package verification"
 gpg --verify raspiBackup_0.7.2.deb.sig raspiBackup_0.7.2.deb
+# Doesn't work :-(( Why???
+# gpg --verify framps.sig raspiBackup_0.7.2.deb
+if (( $? )); then
+	echo "Package verification failed !!!"
+	exit 42
+fi
 show "Install package"
 sudo apt install -y ./raspiBackup_0.7.2.deb
 show "Show installation result"
