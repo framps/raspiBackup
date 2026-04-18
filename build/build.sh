586045c42189388b8c99a305f4b9a82de78dbb09
framp <framp@linux-tips-and-tricks.de>
2026-04-18 20:37:33 +0200

    Added signing

--- build/build.sh
+++ build/build.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+source ./signInit.sh
+sudo apt remove raspibackup
 dpkg-deb --build raspiBackup_0.7.2
+gpg --detach-sign raspiBackup_0.7.2.deb
+gpg --verify raspiBackup_0.7.2.deb.sig raspiBackup_0.7.2.deb
+sudo apt install  ./raspiBackup_0.7.2.deb
+
+
