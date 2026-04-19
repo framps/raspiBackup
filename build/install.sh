9a60ebec6fee2d3b5898240c393272577f2bde46
framp <framp@linux-tips-and-tricks.de>
2026-04-19 16:52:54 +0200

    Download package from github

--- build/install.sh
+++ build/install.sh
@@ -2,7 +2,15 @@
 
 source common.sh
 
+cleanup() {
+	show "Cleanig up"
+	rm -f framps.gpg.asc
+	#rm -f raspiBackup_0.7.2.deb
+	#rm -f raspiBackup_0.7.2.deb.sig
+}
+
 trap 'err $?' ERR
+trap 'cleanup $?' SIGINT SIGTERM SIGHUP EXIT
 
 exec 1> >(stdbuf -i0 -o0 -e0 tee -ia "$LOG_FILE")
 exec 2> >(stdbuf -i0 -o0 -e0 tee -ia "$LOG_FILE" >&2)
@@ -10,13 +18,17 @@ exec 2> >(stdbuf -i0 -o0 -e0 tee -ia "$LOG_FILE" >&2)
 #show "Cleanup installation"
 #sudo apt remove -y raspibackup rsync || true
 
-if ! gpg --list-keys | grep framps; then
+if ! gpg --list-keys | grep -q framps; then
 	show "Retrieve key from github"
 	curl https://github.com/framps.gpg | gpg --yes --dearmor -o framps.gpg.asc
 	show "Import framps key"
 	gpg --import  framps.gpg.asc
-	rm framps.gpg.asc
 fi
+
+show "Downloading package"
+curl -fsSL https://raw.githubusercontent.com/framps/raspiBackup/refs/heads/master/build/raspiBackup_0.7.2.deb -o raspiBackup_0.7.2.deb
+show "Downloading signature"
+curl -fsSL https://raw.githubusercontent.com/framps/raspiBackup/refs/heads/master/build/raspiBackup_0.7.2.deb.sig -o raspiBackup_0.7.2.deb.sig
 
 show "Package verification"
 gpg --verbose --verify raspiBackup_0.7.2.deb.sig raspiBackup_0.7.2.deb
