39cf2cec0fb650f2675c29e11e0bf8099f1caac0
framp <framp@linux-tips-and-tricks.de>
2026-04-21 19:59:31 +0200

    Added beta and release version comparison tests

--- build/compareVersions.sh
+++ build/compareVersions.sh
@@ -1,31 +1,45 @@
 #!/bin/bash
 
+# Check and test version comparison
+
 declare -A m=( [gt]=">" [lt]="<" )
 
 cmp() {
-
-	echo -n "$1 ${m[$2]} $3 "
-	dpkg --compare-versions "$1" "$2" "$3" && echo yes || echo no
+	local result
+	echo -n "$1 ${m[$2]} $3 : "
+	dpkg --compare-versions "$1" "$2" "$3"
+	result=$?
+	(( $result )) && echo "no" || echo "yes"
+	if [[ -n $4 ]]; then
+		if (( $result == 0 )); then
+			echo "Error: no expected"
+			exit 42
+		fi
+	elif (( $result ==  1 )); then
+			echo "Error yes expected"
+			exit 42
+	fi
 }
 
-echo "Nasty feature of ~"
-cmp 1.0~beta lt 1.0
-cmp 1.0~1 lt 1.0
-cmp 1.0~2 gt 1.0~1echo
-echo
-
-echo "Beta replaces a normal version and is replaced by a future version"
+echo "Beta replaces current and RC replaces current version and Beta and is replaced by a future version"
 cmp 1.0 lt 1.0.1
-cmp 1.0-beta gt 1.0
-cmp 1.0-beta lt 1.0.1
+cmp 1.0.1~beta gt 1.0
+cmp 1.0.1~rc1 gt 1.0
+cmp 1.0.1~rc1 gt 1.0.1~beta
+cmp 1.0.1~rc2 gt 1.0.1~beta
+cmp 1.0.1~rc2 gt 1.0.1~rc1
+cmp 1.0.1~rc2 lt 1.0.1
+
 echo
 
-echo "Hotfixes are replaced by future versions"
-cmp 1.0-m-1 gt 1.0
-cmp 1.0-m-1 lt 1.0.1
-cmp 1.0-m-2 lt 1.0.1
+echo "Hotfixes are replaced by future versions and betas and and release candidates"
+cmp 1.0-m1 gt 1.0
+cmp 1.0-m1 lt 1.0.1
+cmp 1.0-m2 lt 1.0.1
+cmp 1.0-m1 lt 1.0.1~beta1
+cmp 1.0-m1 lt 1.0.1~rc1
 echo
 
 echo "Hotfixes have no dependency to other hotfixes"
-cmp 1.0-m-1 lt 1.0-m-2
-cmp 1.0-m-2 lt 1.0-m-1
+cmp 1.0-m1 lt 1.0-m2
+cmp 1.0-m2 lt 1.0-m1 no
