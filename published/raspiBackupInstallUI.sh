410e5f54fe8599f6c23b2fb6e2ec03e09adfa492
framp <framp@linux-tips-and-tricks.de>
2026-08-03 22:14:31 +0200

    Another trial

--- published/raspiBackupInstallUI.sh
+++ published/raspiBackupInstallUI.sh
@@ -108,11 +108,11 @@ MYHOMEURL=readonly MYHOMEURL="https://raw.githubusercontent.com/framps/raspiBack
 
 MYDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-GIT_DATE="$Date$"
+GIT_DATE="$Date: 2026-08-03 22:00:07 +0200$"
 GIT_DATE_ONLY=${GIT_DATE/: /}
 GIT_DATE_ONLY=$(cut -f 2 -d ' ' <<<$GIT_DATE)
 GIT_TIME_ONLY=$(cut -f 3 -d ' ' <<<$GIT_DATE)
-GIT_COMMIT="$Sha1$"
+GIT_COMMIT="$Sha1: 321a995$"
 GIT_COMMIT_ONLY=$(cut -f 2 -d ' ' <<<$GIT_COMMIT | sed 's/\$//')
 
 GIT_CODEVERSION="$MYSELF $VERSION, $GIT_DATE_ONLY/$GIT_TIME_ONLY - $GIT_COMMIT_ONLY"
