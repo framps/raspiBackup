6c6801b2ddbf58f5a13eeb0c57c4f3ea6402f8f7
framp <framp@linux-tips-and-tricks.de>
2026-08-29 21:27:16 +0200

    Use consistent naming for date and sha

--- scripts/raspiBackupOnlineVersions.sh
+++ scripts/raspiBackupOnlineVersions.sh
@@ -8,7 +8,7 @@
 #
 #######################################################################################################################
 #
-#    Copyright (c) 2021-2025 framp at linux-tips-and-tricks dot de
+#    Copyright (c) 2021-2026 framp at linux-tips-and-tricks dot de
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -28,21 +28,20 @@
 #shellcheck disable=SC2034,SC2154
 # (warning): Date is referenced but not assigned (did you mean 'DATE'?).
 # (warning): GIT_DATE appears unused. Verify use (or export if used externally).
-GIT_DATE="$Date$"
+GIT_DATE="$Date: 2026-08-04 18:52:24 +0200$"
 #shellcheck disable=SC2034,SC2154
 # (warning): Sha1 is referenced but not assigned (did you mean 'DATE'?).
 # (warning): GIT_COMMIT appears unused. Verify use (or export if used externally).
-GIT_COMMIT="$Sha1$"
+GIT_COMMIT="$Sha1: 527812d$"
 
-MYHOMEDOMAIN="www.linux-tips-and-tricks.de"
-MYHOMEURL="https://$MYHOMEDOMAIN"
-DOWNLOAD_URL="$MYHOMEURL/raspiBackup/raspiBackup.sh"
-BETA_DOWNLOAD_URL="$MYHOMEURL/raspiBackup/beta/raspiBackup.sh"
-INSTALLER_DOWNLOAD_URL="$MYHOMEURL/raspiBackup/raspiBackupInstallUI.sh"
-INSTALLER_BETA_DOWNLOAD_URL="$MYHOMEURL/raspiBackup/beta/raspiBackupInstallUI.sh"
-PROPERTIES_DOWNLOAD_URL="$MYHOMEURL/raspiBackup/raspiBackup.properties"
-CONF_DE_DOWNLOAD_URL="$MYHOMEURL/raspiBackup/raspiBackup_de.conf"
-CONF_EN_DOWNLOAD_URL="$MYHOMEURL/raspiBackup/raspiBackup_en.conf"
+MYHOMEURL="https://raw.githubusercontent.com/framps/raspiBackup/master/published"
+DOWNLOAD_URL="$MYHOMEURL/raspiBackup.sh"
+BETA_DOWNLOAD_URL="$MYHOMEURL//beta/raspiBackup.sh"
+INSTALLER_DOWNLOAD_URL="$MYHOMEURL//raspiBackupInstallUI.sh"
+INSTALLER_BETA_DOWNLOAD_URL="$MYHOMEURL/beta/raspiBackupInstallUI.sh"
+PROPERTIES_DOWNLOAD_URL="$MYHOMEURL/raspiBackup.properties"
+CONF_DE_DOWNLOAD_URL="$MYHOMEURL/raspiBackup_de.conf"
+CONF_EN_DOWNLOAD_URL="$MYHOMEURL/raspiBackup_en.conf"
 
 DOWNLOAD_TIMEOUT=60 # seconds
 DOWNLOAD_RETRIES=3
@@ -57,38 +56,37 @@ function analyze() { # fileName url
 	tmp=$(mktemp)
 	wget "$2" -q --tries="$DOWNLOAD_RETRIES" --timeout="$DOWNLOAD_TIMEOUT" -O "$tmp"
 
-	# GIT_COMMIT="$Sha1$"
-	sha="$(grep "^GIT_COMMIT=" "$tmp" | cut -f 2 -d ' '| sed  -e "s/[\$\"\']//g")"
-	if [[ -z "$sha" ]]; then
-		sha="$(grep "GIT_COMMIT=" "$tmp" | cut -f 3-4 -d ' ' )"
-	fi
-	if [[ -z "$sha" ]]; then
-		sha="$(grep "$SHA" "$tmp" | cut -f 3-4 -d ' ' )"
-	fi
+	# GIT_COMMIT='527812d'
 
-	#shellcheck disable=SC2001
-	#(style): See if you can use ${variable//search/replace} instead.
-	sha="$(sed  -e "s/[\$\"]//g" <<< "$sha")"
-
+	local pattern="GIT_COMMIT=['\"]([^\"]+)['\"]"
+	sha="$(grep "GIT_COMMIT=" "$tmp")"
+	if [[ "$sha" =~ $pattern ]]; then
+		sha=${BASH_REMATCH[1]}
+	else
+		sha="ukn"
+	fi
+	
 	# VERSION="0.6.5-beta"	# -beta, -hotfix or -dev suffixes possible
-	version="$(grep -e "^VERSION=" "$tmp" | cut -f 2 -d = | sed  -e "s/\"//g" -e "s/[[:space:]]*#.*//")"
-	if [[ -z "$version" ]]; then
-		version="$(grep -e "^VERSION_CONFIG=" "$tmp" | cut -f 2 -d = | sed  -e "s/\"//g" -e "s/[[:space:]]*#.*//")"
+	local pattern="(VERSION|VERSION_CONFIG)=['\"]([^ ]+)['\"]"
+	version="$(grep -E "(VERSION|VERSION_CONFIG)=" "$tmp")"
+	if [[ "$version" =~ $pattern ]]; then
+		version="${BASH_REMATCH[2]}"
+	else 
+		version="ukn"
 	fi
 
-	# GIT_DATE="$Date$"
-	date="$(grep "^GIT_DATE=" "$tmp" | cut -f 2-3 -d ' ' )"
-	if [[ -z "$date" ]]; then
-		date="$(grep "GIT_DATE=" "$tmp" | cut -f 3-4 -d ' ' )"
-	fi
-	if [[ -z "$date" ]]; then
-		date="$(grep "$DATE" "$tmp" | cut -f 3-4 -d ' ' )"
+	# GIT_DATE="2026-08-29 12:59:01 +0200"
+	local pattern="GIT_DATE=['\"]([^\"']+)['\"]"
+	date="$(grep -e "GIT_DATE=" "$tmp")"
+	if [[ "$date" =~ $pattern ]]; then
+		date="${BASH_REMATCH[1]}"
+	else 
+		date="ukn"
 	fi
 
 	[[ -z "$version" ]] && version="N/A"
 	[[ -z "$sha" ]] && sha="N/A"
 	[[ -z "$date" ]] && date="N/A"
-		
 
 	printf "%-30s: Version: %-10s Date: %-20s Sha: %-10s\n" "$1" "$version" "$date" "$sha"
 	rm "$tmp"
